e2ef6d9f6bd10947cd58c16f07c8111f968338b2
3picKillz <chris974@live.com>
2012-12-19 19:42:53 -0500

    Updated Player Connection

---
+++ MCDelta Plus_/Players/Player.Colors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDelta_Plus
+{
+    class PlayerColors
+    {
+       
+    }
+}
---
+++ MCDelta Plus_/Players/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace MCDelta_Plus
+{
+    class Player
+    {
+        public string Username { get; set;}
+        public string ID { get; set;}
+        public string IP { get; set;}
+        public bool Op = false;
+        public bool Banned = false;       
+        
+        public Socket socket;
+        internal Player(TcpClient tcpclient)
+        {
+            Server.Log(Username + "Has just Joined The Server");
+            socket = tcpclient.Client;
+            try
+            {
+               
+            }
+            catch { }
+        }
+    }
+}
