4035e88b5f5e9f769940ab9dca21d3de4d1b0551
3picKillz <chris974@live.com>
2013-01-02 11:55:35 -0500

    Heartbeat And Map Generator Arguements

---
+++ MCDelta Plus_/GeneratorArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCDelta_Plus
+{
+    class GeneratorArgs
+    {
+        public int MaxDepth { get; set; }
+        public int MinDepth { get; set; }
+    }
+}
---
+++ MCDelta Plus_/Heart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MCDelta_Plus
+{
+    public abstract class Heart
+    {
+       public static List<Heart> hearts = new List<Heart>();
+		public abstract string URL { get; }
+		public abstract string Prepare();
+		public virtual string OnPump(StreamReader responseStreamReader)
+		{
+			string line = ""; int i = 0;
+			string URL = "";
+
+            while (line != null)
+            {
+                i++;
+                line = responseStreamReader.ReadLine();
+                if (line != null)
+                    URL = line;
+            }
+            responseStreamReader.Close();
+            return URL;
+		}
+	}    
+}
